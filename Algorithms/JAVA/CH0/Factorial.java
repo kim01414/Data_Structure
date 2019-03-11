c00de31e67d7644146939b948c9bffb80342490f
kim01414 <39479749+kim01414@users.noreply.github.com>
2019-03-11 12:01:02 +0900

    Update Factorial.java

--- Algorithms/JAVA/CH0/Factorial.java
+++ Algorithms/JAVA/CH0/Factorial.java
@@ -1,7 +1,7 @@
 public class Factorial{
     public static int factorial(int n){
         if(n<=1) return 1;
-        else return factorial(n-1)*n;
+        return factorial(n-1)*n;
     }
     public static void main(String args[]){
         int num = 5;
